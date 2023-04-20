4b2cd25b3007c9d4b9e508caba51aa34cd2fa47b
Ricardo Bion <ricardobion@gmail.com>
2023-04-20 09:16:07 -0700

    Revert "Retain name of grouping variable"

--- R/CalculateGroupPath.R
+++ R/CalculateGroupPath.R
@@ -9,39 +9,35 @@
 #' @source 
 #' Code adapted from a solution posted by Tony M to \url{http://stackoverflow.com/questions/9614433/creating-radar-chart-a-k-a-star-plot-spider-plot-using-ggplot2-in-r}.
 CalculateGroupPath <- function(df) {
-  # Drop dead levels. This might happen if the data is filtered on the way
-  # into ggradar.
-  path <- forcats::fct_drop(df[, 1])
-  # set the name of the variable that will be used for grouping
-  theGroupName <- colnames(df)[1]
+  path <- df[, 1]
   
   ## find increment
-  nPathPoints <- ncol(df) - 1
-  angles <- seq(from = 0, to = 2 * pi, by = (2 * pi) / nPathPoints)
+  angles <- seq(from = 0, to = 2 * pi, by = (2 * pi) / (ncol(df) - 1))
   ## create graph data frame
-  nDataPoints <- ncol(df) * length(levels(path))
-  graphData <- data.frame(
-                          seg = rep("",nDataPoints),
-                          x = rep(0, nDataPoints),
-                          y = rep(0, nDataPoints))
-  colnames(graphData)[1] <- theGroupName
-
-  rowNum <- 1
-  for (i in 1:length(levels(path))) {
-    pathData <- subset(df, df[, 1] == levels(path)[i])
+  graphData <- data.frame(seg = "", x = 0, y = 0)
+  graphData <- graphData[-1, ]
+  
+  for (i in levels(path)) {
+    pathData <- subset(df, df[, 1] == i)
     for (j in c(2:ncol(df))) {
-      graphData[rowNum,theGroupName] <- levels(path)[i]
-      graphData$x[rowNum] <- pathData[, j] * sin(angles[j - 1])
-      graphData$y[rowNum] <- pathData[, j] * cos(angles[j - 1])
-      rowNum <- rowNum + 1
+      # pathData[,j]= pathData[,j]
+      
+      
+      graphData <- rbind(graphData, data.frame(
+        group = i,
+        x = pathData[, j] * sin(angles[j - 1]),
+        y = pathData[, j] * cos(angles[j - 1])
+      ))
     }
     ## complete the path by repeating first pair of coords in the path
-    graphData[rowNum,theGroupName] <- levels(path)[i]
-    graphData$x[rowNum] <- pathData[, 2] * sin(angles[1])
-    graphData$y[rowNum] <- pathData[, 2] * cos(angles[1])
-    rowNum <- rowNum + 1
+    graphData <- rbind(graphData, data.frame(
+      group = i,
+      x = pathData[, 2] * sin(angles[1]),
+      y = pathData[, 2] * cos(angles[1])
+    ))
   }
   # Make sure that name of first column matches that of input data (in case !="group")
-  graphData[,1] <- factor(graphData[,1], levels=levels(path) ) # keep group order
+  colnames(graphData)[1] <- colnames(df)[1]
+  graphData$group <- factor(graphData$group, levels=levels(df[, 1]) ) # keep group order
   graphData # data frame returned by function
 }
